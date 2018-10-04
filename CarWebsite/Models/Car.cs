a803c4a69e4198e2db9af45684f3ca83d6298e9f
21701763 <21701763@bucks.ac.uk>
2018-10-04 13:09:54 +0100

    Modified site master

---
+++ CarWebsite/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarWebsite.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
